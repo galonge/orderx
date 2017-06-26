e09f66d1b5f23a40ba5912c3bf84b0403f990192
George Alonge <georgedelson@gmail.com>
2017-06-26 13:06:10 +0100

    add node modules for sass and modify htaccess

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+
         <title>{{config('app.name', 'orderX')}}</title>
 
         <!-- Fonts -->
@@ -13,6 +15,8 @@
       
     </head>
     <body>
+        <div class="container">
             @yield('content')
+        </div>
     </body>
 </html>
