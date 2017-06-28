6bf397de8fb1452baa0dd4bbc89f39ca6af64fe6
George Alonge <georgedelson@gmail.com>
2017-06-28 09:04:36 +0100

    modify readme to include details

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -5,6 +5,6 @@
 	  <div class="jumbotron text-center">
 	     <h1> Welcome to orderX!</h1>
 	     <p>A simple order management system</p>
-	     <p><a class="btn btn-info btn-sm" href="/login" role="button">Login</a> <a class="btn btn-success btn-sm" href="/register" role="button">Register</a>
+	     <p><a class="btn btn-info btn-sm" href="/orderx/login" role="button">Login</a> <a class="btn btn-success btn-sm" href="/orderx/register" role="button">Register</a>
 	   </div>
 	@endsection
